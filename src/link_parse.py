542f61b0a838b809a3ff098202e6bf4ec71b0028
venzy <david.venz@gmail.com>
2024-11-09 21:00:43 +1000

    Ch 3 Inline L2-4 Extract Links

---
+++ src/link_parse.py
@@ -0,0 +1,11 @@
+import re
+
+image_re = re.compile(r"\!\[(.*?)\]\((.*?)\)")
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    images: list[tuple[str, str]] = image_re.findall(text)
+    return images
+
+link_re = re.compile(r"[^!]\[(.*?)\]\((.*?)\)")
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    links: list[tuple[str, str]] = link_re.findall(text)
+    return links
---
+++ src/test_link_parse.py
@@ -0,0 +1,38 @@
+import unittest
+
+from link_parse import *
+
+
+class TestLinkParse(unittest.TestCase):
+    def test_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        extracted = extract_markdown_images(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(extracted, expected)
+
+    def test_image_no_alt_text(self):
+        text = "Surprise... ![](https://i.imgur.com/aKaOqIh.gif)"
+        extracted = extract_markdown_images(text)
+        expected = [("", "https://i.imgur.com/aKaOqIh.gif")]
+        self.assertEqual(extracted, expected)
+
+    def test_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        extracted = extract_markdown_links(text)
+        expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(extracted, expected)
+
+    def test_images_and_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev) and with a ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+
+        extracted = extract_markdown_links(text)
+        expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(extracted, expected)
+
+        extracted = extract_markdown_images(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif")]
+        self.assertEqual(extracted, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
