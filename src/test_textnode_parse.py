b9da71a941c6f6136c2ba67b09c1fc549bf9a5b7
venzy <david.venz@gmail.com>
2024-11-09 21:54:55 +1000

    Ch 3 Inline L6 Text to TextNodes

--- src/test_textnode_parse.py
+++ src/test_textnode_parse.py
@@ -113,6 +113,22 @@ class TestTextNodeParse(unittest.TestCase):
         new_nodes = split_nodes_image(old_nodes)
         self.assertEqual(new_nodes, old_nodes)
 
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(nodes, expected)
  
 if __name__ == "__main__":
     unittest.main()
--- src/textnode_parse.py
+++ src/textnode_parse.py
@@ -74,3 +74,14 @@ def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
                     new_nodes.append(TextNode(remainder, TextType.TEXT))
     
     return new_nodes
+
+def text_to_textnodes(text: str) -> list[TextNode]:
+    nodes: list[TextNode] = [TextNode(text, TextType.TEXT)]
+
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+    return nodes
